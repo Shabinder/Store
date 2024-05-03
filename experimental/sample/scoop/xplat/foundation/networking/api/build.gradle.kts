e90c3d27c1b6c467fa0f2a4b7edc4796a3384d9b
Matt Ramotar <matt.ramotar@uber.com>
2024-05-03 08:27:06 -0400

    Add foundation modules
    Signed-off-by: Matt Ramotar <matt.ramotar@uber.com>

---
+++ experimental/sample/scoop/xplat/foundation/networking/api/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("plugin.octonaut.android.library")
+    id("plugin.octonaut.kotlin.multiplatform")
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.compose)
+    id("com.apollographql.apollo3") version "3.8.2"
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                api(compose.runtime)
+                api(compose.components.resources)
+                api(libs.circuit.foundation)
+                api(libs.apollo.runtime)
+                api(libs.kotlinx.serialization.core)
+                api(project(":experimental:sample:scoop:xplat:foundation:di:api"))
+                api("io.github.pdvrieze.xmlutil:serialization:0.86.3")
+
+            }
+        }
+    }
+}
+
+android {
+    namespace = "monster.scoop.xplat.foundation.networking.api"
+}
+
+apollo {
+    service("service") {
+        packageName.set("monster.scoop.xplat.foundation.networking.api")
+        schemaFiles.from(file("src/commonMain/graphql/schema.graphqls"))
+    }
+}
---
+++ experimental/sample/scoop/xplat/foundation/networking/impl/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("plugin.octonaut.android.library")
+    id("plugin.octonaut.kotlin.multiplatform")
+    alias(libs.plugins.serialization)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.plugin.parcelize)
+}
+
+kotlin {
+    androidTarget {
+        plugins.apply(libs.plugins.paparazzi.get().pluginId)
+    }
+
+    sourceSets {
+
+        commonMain {
+            dependencies {
+                implementation(compose.material3)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlinInject.runtime)
+                implementation(libs.ktor.core)
+                implementation(libs.ktor.negotiation)
+                implementation(libs.ktor.serialization.json)
+                implementation(libs.kotlinx.serialization.core)
+                api(project(":experimental:sample:octonaut:xplat:foundation:networking:api"))
+                api(project(":experimental:sample:octonaut:xplat:foundation:di:api"))
+                implementation("io.github.pdvrieze.xmlutil:serialization:0.86.3")
+            }
+        }
+
+        androidUnitTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        androidMain {
+            dependencies {
+                implementation("io.ktor:ktor-client-okhttp-jvm:2.3.7")
+            }
+        }
+        nativeMain {
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.7")
+            }
+        }
+        jvmMain {
+            dependencies {
+                implementation("io.ktor:ktor-client-apache5:2.3.7")
+            }
+        }
+    }
+}
+
+dependencies {
+    add("kspAndroid", libs.kotlinInject.compiler)
+    add("kspIosX64", libs.kotlinInject.compiler)
+    add("kspIosArm64", libs.kotlinInject.compiler)
+}
+
+android {
+    namespace = "monster.scoop.xplat.foundation.networking.impl"
+}
